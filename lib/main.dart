1831d29a611933efcd4a45a58e7297b0ec6e5b34
VojtechPawlik <vojta.pawlik@seznam.cz>
2025-11-14 18:43:15 +0100

    mainpage_change

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,6 @@
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:strike_app/firebase_options.dart';
-import 'screens/main_screen.dart';
 import 'screens/splash_screen.dart';
 import 'services/theme_service.dart';
 import 'services/localization_service.dart';
